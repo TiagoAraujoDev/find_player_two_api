ae9650d5bcefad1e102e6765ebf34d164bf258e8
Tiago Araujo <tiagomuniz130@gmail.com>
2023-10-28 11:44:55 -0300

    feat: create user endpoint

---
+++ src/http/controller/createUserController.ts
@@ -0,0 +1,39 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { UsernameAlreadyRegisterError } from "~/useCases/error/UsernameAlreadyRegister";
+import { makeCreateUserUseCase } from "~/useCases/factory/makeCreateUserUseCase";
+
+async function create(request: FastifyRequest, reply: FastifyReply) {
+  const bodySchema = z.object({
+    username: z.string(),
+    age: z.coerce.number(),
+    country: z.string(),
+    description: z.string(),
+  });
+
+  const { username, age, country, description } = bodySchema.parse(
+    request.body,
+  );
+
+  try {
+    const createUserUseCase = makeCreateUserUseCase();
+    const user = await createUserUseCase.execute({
+      username,
+      age,
+      country,
+      description,
+    });
+
+    return reply.status(201).send(user);
+  } catch (error) {
+    if (error instanceof UsernameAlreadyRegisterError) {
+      return reply.status(400).send({ message: error.message });
+    }
+
+    // This will capture any unexpected error
+    throw error;
+  }
+}
+
+export { create };
--- src/http/controller/user/route.ts
+++ src/http/controller/user/route.ts
@@ -1,10 +1,9 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { FastifyInstance } from "fastify";
+
+import { create } from "../createUserController";
 
 export async function usersRoutes(app: FastifyInstance) {
   
-  app.get("/users", async (request: FastifyRequest, reply: FastifyReply) => {
-    console.log(request.url);
-    return reply.code(200).send({ name: "tiago", age: 29 });
-  });
+  app.post("/users", create);
   
-} 
+}
